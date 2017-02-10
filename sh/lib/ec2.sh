a72c565a26e82a142488f650022532fb8fbeeebe
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-02-10 09:36:30 +0000

    Add logic to set AWS_DEFAULT_REGION if needed

--- sh/lib/ec2.sh
+++ sh/lib/ec2.sh
@@ -13,11 +13,18 @@ getRegion() {
 
 getPublicIP() {
   #ARG1: Optional instance-id. If instance ID is not provided use getInstanceId to read it from meta-data
+  #ARG2: Optional region. If region is not provided and envrionment variable AWS_DEFAULT_REGION is not set
+  #      attempt to resolve it by querying instance meta-data
   #returns: Public IP address if the instance
   if [[ -z $1 ]]; then
     INSTANCEID=$(getInstanceId)
   else
     INSTANCEID=$1
+  fi
+  if [[ -z $2 && -z ${AWS_DEFAULT_REGION} ]]; then
+    AWS_DEFAULT_REGION=$(getRegion)
+  else
+    AWS_DEFAULT_REGION=$2
   fi
   aws ec2 describe-instances --instance-ids ${INSTANCEID}  --query 'Reservations[].Instances[].PrivateIpAddress' --output text
 }
